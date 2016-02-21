0ee75197ef74442d3bbb76dcba57bb872f6e7050
AuthorOfTheSurf <zanekansil@gmail.com>
2016-02-21 13:44:55 -0800

    Initialize knockout view model from Jade side

---
+++ js/homepage.ko.js
@@ -0,0 +1,14 @@
+// Script for the homepage, this is mostly knockout code.
+// I use the .knockout.js convention to signal that the script is
+// mostly knockout code, i.e. a ViewModel
+
+// In this example, data is expected to be the same JSON blob that was given
+// to the Jade template.
+var HomepageViewModel = function (data) {
+  var self = this;
+
+  self.title = ko.observable(data.title);
+  self.subtitle = ko.observable(data.subtitle);
+  // data.subtitle is empty! Lets put an actual subtitle there!
+  self.subtitle("I am a subtitle written purely from the Knockout-side!");
+};
