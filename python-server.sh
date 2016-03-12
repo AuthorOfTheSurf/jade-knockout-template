3900d64bfab10eb51183009dc9914c0468460e6c
AuthorOfTheSurf <zanekansil@gmail.com>
2016-03-12 00:05:47 -0800

    Tweaks

--- python-server.sh
+++ python-server.sh
@@ -2,6 +2,6 @@
 
 # Start a python server to serve up the html locally. Without this you would
 # have to double-click the html files. This is closer to what the development
-# environment would be like. Requires python 2.7
+# environment would be like. Requires Python 2.7
 # I recommend running this in its own terminal window/tab
 python -m SimpleHTTPServer 8000
