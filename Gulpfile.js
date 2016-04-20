9c08f7552cc463d2d9951e32510d1b94527bcdf8
Edward Bramanti <edward@bramanti.org>
2016-04-19 23:59:44 -0700

    Update jade pipeline to parse from data folder
    Jade files will now have a matching file in the /data folder (will be
    created if it DNE), and they will be interpolated in general data
    rather than the locals object.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,22 @@
 const gulp = require('gulp');
 const fs = require('fs');
+const path = require('path');
 const connect = require('gulp-connect');
 const jade = require('gulp-jade');
 const rename = require('gulp-rename');
+const data = require('gulp-data');
+
+const parseJSONData = (file) => {
+  const dataFilePath = `./data/${path.basename(file.path, '.jade')}.json`;
+  // Check for corresponding data file, create if it DNE.
+  try {
+    fs.accessSync(dataFilePath, fs.F_OK);
+  } catch(e) {
+    fs.writeFileSync(dataFilePath, '{}', 'utf8');
+  }
+
+  return JSON.parse(fs.readFileSync(`./data/${path.basename(file.path, '.jade')}.json`, 'utf8'))
+};
 
 gulp.task('connect', () => {
   connect.server({
@@ -14,14 +28,14 @@ gulp.task('connect', () => {
 
 gulp.task('jade', () => {
   gulp.src('jade/*.jade')
-    .pipe(jade({
-      locals: JSON.parse(fs.readFileSync('./data-source.json', 'utf8')),
-    }))
+    .pipe(data(parseJSONData))
+    .pipe(jade())
     .pipe(rename({ dirname: '' }))
     .pipe(gulp.dest('build/html'))
     .pipe(connect.reload());
 
   gulp.src('index.jade')
+    .pipe(data(parseJSONData))
     .pipe(jade())
     .pipe(gulp.dest('build'));
 });
@@ -30,8 +44,8 @@ gulp.task('watch-jade', () => {
   gulp.watch(['**/*.jade'], ['jade']);
 });
 
-gulp.task('watch-data-source', () => {
-  gulp.watch('data-source.json', ['jade']);
+gulp.task('watch-data', () => {
+  gulp.watch('data/**', ['jade']);
 });
 
 gulp.task('copy-js', () => {
@@ -44,5 +58,5 @@ gulp.task('watch-js', () => {
   gulp.watch(['js/**/*'], ['copy-js']);
 });
 
-gulp.task('watch', ['watch-jade', 'watch-js', 'watch-data-source']);
+gulp.task('watch', ['watch-jade', 'watch-js', 'watch-data']);
 gulp.task('default', ['jade', 'copy-js', 'connect', 'watch']);
